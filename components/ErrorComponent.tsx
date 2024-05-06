da0769d95fd44e7efcc93a2236c52438a396d0f8
Lawal Abubakar Babatunde <utility4all@gmail.com>
2024-05-06 21:40:06 +0100

    Feat: added error_boundary with toast

--- components/ErrorComponent.tsx
+++ components/ErrorComponent.tsx
@@ -1,23 +1,5 @@
 'use client';
 import React from 'react';
-// import { toast } from '@/components/ui/use-toast';
-
-// export default function ErrorComponent({ error, resetErrorBoundary }) {
-
-//     return (
-//         <div>
-//             {
-//                 <>
-//                     {toast({
-//                         title: 'An error occurred',
-//                         description: error.message,
-//                     })}
-//                 </>
-//             }
-//         </div>
-//     )
-// }
-
 import withToast from './withToast';
 
 interface ErrorBoundaryProps {
@@ -39,7 +21,6 @@ class ErrorBoundary extends React.Component<
     super(props);
     this.state = { hasError: false };
     this.errorListener = event => {
-      console.log('errorListener', event);
       this.props.toast({
         title: 'An error occurred',
         description: event.message,
@@ -48,19 +29,15 @@ class ErrorBoundary extends React.Component<
   }
 
   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
-    console.log('Error caught in getDerivedStateFromError:', error);
     return { hasError: true };
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
-    console.log('Error caught by ErrorBoundary:', error);
-    console.log('Error info:', errorInfo);
     this.setState({ hasError: true });
     this.props.toast({
       title: 'An error occurred',
       description: error.message,
     });
-    console.log({ error, errorInfo });
   }
 
   componentDidMount(): void {
@@ -72,16 +49,6 @@ class ErrorBoundary extends React.Component<
   }
 
   render() {
-    // if (this.state.hasError) {
-    //   return (
-    //     <div>
-    //       <h2>Oops, there is an error!</h2>
-    //       <button onClick={() => this.setState({ hasError: false })}>
-    //         Try again?
-    //       </button>
-    //     </div>
-    //   );
-    // }
     return this.props.children;
   }
 }
