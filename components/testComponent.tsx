a29321c5b451e05d02847c7a9fd45553b5291d83
Lawal Abubakar Babatunde <utility4all@gmail.com>
2024-05-15 14:47:08 +0100

    chore: Removed error test component

--- components/testComponent.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-export default function TestComponent() {
-    console.log('Test Component')
-    setTimeout(() => {throw new Error('crash')}, 1000)
-    return <div>Test Component</div>;
-}
